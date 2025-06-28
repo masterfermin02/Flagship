8efa0f6f45bfef97d60b3dc11fb7ccbca75e3136
Ivan Mercedes <ivanmercede@gmail.com>
2025-06-27 22:50:34 -0400

    Implement Blade directive and middleware

--- src/Middleware/FlagshipMiddleware.php
+++ src/Middleware/FlagshipMiddleware.php
@@ -1,3 +1,19 @@
 <?php
 
 namespace Flagship\Middleware;
+
+use Closure;
+use Flagship\Facades\Flagship;
+use Illuminate\Http\Request;
+
+class FlagshipMiddleware
+{
+    public function handle(Request $request, Closure $next, string $flag)
+    {
+        if (!Flagship::isEnabled($flag, $request->user())) {
+            abort(404);
+        }
+
+        return $next($request);
+    }
+}
