3d40d20a72069d286ba7e2dc6b8cf91f1bf48238
Ivan Mercedes <ivanmercede@gmail.com>
2025-06-28 01:12:29 -0400

    feat: register Blade directive and console commands

--- src/FlagshipServiceProvider.php
+++ src/FlagshipServiceProvider.php
@@ -11,28 +11,51 @@ use Illuminate\Support\ServiceProvider;
 
 class FlagshipServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the package's services.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/flagship.php', 'flagship'
+            __DIR__ . '/../config/flagship.php',
+            'flagship'
         );
 
         $this->app->singleton(FlagshipInterface::class, FlagshipService::class);
         $this->app->alias(FlagshipInterface::class, 'flagship');
     }
 
+    /**
+     * Bootstrap the package's services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/flagship.php' => config_path('flagship.php'),
+            __DIR__ . '/../config/flagship.php' => config_path('flagship.php'),
         ], 'flagship-config');
 
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'flagship-migrations');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-       
+        $this->callAfterResolving('blade.compiler', function ($blade) {
+            $blade->if('flagship', function ($flag, $user = null) {
+                 return resolve(FlagshipInterface::class)->isEnabled($flag, $user);
+            });
+        });
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                FlagshipMakeCommand::class,
+                FlagshipListCommand::class,
+                FlagshipToggleCommand::class,
+            ]);
+        }
     }
 }
