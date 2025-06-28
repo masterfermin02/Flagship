59ea62fe6b461678941adbbc1df1b03667727d90
Ivan Mercedes <ivanmercede@gmail.com>
2025-06-28 10:37:57 -0400

    test: add FeatureFlag factory and FlagshipMakeCommand tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,2 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,75 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Flagship\FlagshipServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends OrchestraTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // config()->set('database.default', 'testbench');
+        // config()->set('database.connections.testbench', [
+        //     'driver'   => 'sqlite',
+        //     'database' => ':memory:',
+        //     'prefix'   => '',
+        // ]);
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // $this->artisan('migrate', ['--database' => 'testbench'])->run();
+
+
+        // Manually register factories
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            $namespace = 'Database\\Factories\\';
+
+            // For package specific models:
+            if (str_starts_with($modelName, 'Flagship\\Models\\')) {
+                $modelName = class_basename($modelName);
+                return 'Flagship\\Factories\\' . $modelName . 'Factory';
+            }
+
+            // Default Laravel app models (if any for testing)
+            $modelName = str_replace('App\\Models\\', '', $modelName);
+            return $namespace . $modelName . 'Factory';
+        });
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FlagshipServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Flagship' => \Flagship\Facades\Flagship::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Setup other necessary configurations for your package
+        $app['config']->set('flagship.cache_enabled', false); // Disable cache for tests by default
+        $app['config']->set('flagship.default_state', false);
+    }
 }
