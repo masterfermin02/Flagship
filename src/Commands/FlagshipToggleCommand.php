c482f826fe8249472bba3d3072acc86dacd6d545
Ivan Mercedes <ivanmercede@gmail.com>
2025-06-28 00:12:07 -0400

    Enhance Flagship commands and service with feature flag management

--- src/Commands/FlagshipToggleCommand.php
+++ src/Commands/FlagshipToggleCommand.php
@@ -1,3 +1,21 @@
 <?php
 
 namespace Flagship\Commands;
+
+use Flagship\Facades\Flagship;
+use Illuminate\Console\Command;
+
+class FlagshipToggleCommand extends Command
+{
+    protected $signature = 'flagship:toggle {name}';
+    protected $description = 'Toggle the state of a feature flag';
+
+    public function handle()
+    {
+        $name = $this->argument('name');
+        
+        Flagship::toggle($name);
+        
+        $this->info("Feature flag '{$name}' toggled");
+    }
+}
